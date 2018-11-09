ef5c341c8ad48c275278bce483092945638b8a33
Alan Goldman <alan.goldman@citrix.com>
2018-11-09 16:45:06 -0500

    Updating with AzureRM failure

--- PartnerTest/Program.cs
+++ PartnerTest/Program.cs
@@ -1,18 +1,22 @@
-﻿
-using System;
+﻿using System;
 using System.Configuration;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Azure.ActiveDirectory.GraphClient;
+using Microsoft.Azure.Management.ResourceManager;
 using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.Rest;
 
 namespace PartnerTest
 {
     public class Program
     {
-        private const string BootstrapAppId = "9f11594c-d487-4572-a986-5dd44f8a6aa2";
-        private const string TenantDirectoryId = "d945addd-74f3-491f-8cc5-5ac214b5a105"; // alantest9.onmicrosoft.com
+        private const string BootstrapAppId = "625a0b73-0843-470f-a518-682e83c28b0e";
+        private const string TenantDirectoryId = "c7b812ca-5b1f-4d55-b73d-937d3a4eab0e"; // alantest5.onmicrosoft.com
+        private const string SubscriptionId = "80404868-E176-4696-9235-071A1E057B6F";
 
         private const string GraphResource = "https://graph.windows.net/";
+        private const string ArmResource = "https://management.azure.com/";
 
         internal static async Task<AuthenticationResult> GetAppAccessToken(string appId, string appSecret, string tenantId, string resource)
         {
@@ -27,12 +31,28 @@ namespace PartnerTest
             var graphClient = new ActiveDirectoryClient(new Uri(new Uri(GraphResource), TenantDirectoryId),
                 async () => (await GetAppAccessToken(BootstrapAppId, ConfigurationManager.AppSettings["BootstrapAppSecret"], TenantDirectoryId, GraphResource)).AccessToken);
 
+            Console.WriteLine("Testing AzureAD api: list service principals, create one for the bootstrap app");
             try
             {
-                var apps = await graphClient.Applications.ExecuteAsync();
-                foreach (var app in apps.CurrentPage)
+                var sps = await graphClient.ServicePrincipals.ExecuteAsync();
+                if (!sps.CurrentPage.Any())
                 {
-                    Console.WriteLine(app.AppId);
+                    Console.WriteLine("No service principals found");
+                }
+                foreach (var sp in sps.CurrentPage)
+                {
+                    Console.WriteLine(sp.AppId);
+                }
+
+                // Without this step the error is:
+                // The received access token is not valid: at least one of the claims 'puid' or 'altsecid' or 'oid' should be present. If you are accessing as application please make sure service principal is properly created in the tenant.
+                if (!sps.CurrentPage.Any(sp => sp.AppId == BootstrapAppId))
+                {
+                    Console.WriteLine("Creating service principal for bootstrap app");
+                    await graphClient.ServicePrincipals.AddServicePrincipalAsync(new ServicePrincipal
+                    {
+                        AppId = BootstrapAppId
+                    });
                 }
             }
             catch (Exception e)
@@ -40,6 +60,42 @@ namespace PartnerTest
                 Console.WriteLine(e.InnerException.Message);
                 throw;
             }
+
+            var rmClient = new ResourceManagementClient(new Uri(ArmResource),
+                new TokenCredentials((await GetAppAccessToken(BootstrapAppId,
+                        ConfigurationManager.AppSettings["BootstrapAppSecret"], TenantDirectoryId, ArmResource))
+                    .AccessToken))
+            {
+                SubscriptionId = SubscriptionId
+            };
+
+            Console.WriteLine("Testing AzureRM api: list resource groups");
+            try
+            {
+                var rgs = await rmClient.ResourceGroups.ListAsync();
+                if (rgs == null || !rgs.Any())
+                {
+                    Console.WriteLine("No resource groups found");
+                }
+                else
+                {
+                    foreach (var rg in rgs)
+                    {
+                        Console.WriteLine(rg.Name);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                if (e.InnerException != null)
+                {
+                    Console.WriteLine(e.InnerException.Message);
+                }
+                throw;
+            }
+
+            Console.ReadKey();
         }
     }
 }
--- README.md
+++ README.md
@@ -1,36 +1,15 @@
 Setup steps:
+0. Have a tenant in the partner with an Azure subscription. The subscription should have a role assignment for "Foreign Principal"
 1. Create an application in the partner directory
 2. Change it to multitenant
 3. Go into its manifest and change the requiredResourceAccess to:
 ```
-"requiredResourceAccess": [
-    {
-      "resourceAppId": "00000002-0000-0000-c000-000000000000",
-      "resourceAccess": [
-        {
-          "id": "311a71cc-e848-46a1-bdf8-97ff7156d8e6",
-          "type": "Scope"
-        },
-        {
-          "id": "a42657d6-7f20-40e3-b6f0-cee03008a62a",
-          "type": "Scope"
-        }
-      ]
-    },
-    {
-      "resourceAppId": "797f4846-ba00-4fd7-ba43-dac1f8f63013",
-      "resourceAccess": [
-        {
-          "id": "41094075-9dad-400e-a0bd-54e686782033",
-          "type": "Scope"
-        }
-      ]
-    },
+  "requiredResourceAccess": [
     {
       "resourceAppId": "00000003-0000-0000-c000-000000000000",
       "resourceAccess": [
         {
-          "id": "7ab1d382-f21e-4acd-a863-ba3e13f7da61",
+          "id": "09850681-111b-4a89-9bed-3f2cae46d706",
           "type": "Role"
         },
         {
@@ -38,15 +17,24 @@ Setup steps:
           "type": "Role"
         },
         {
-          "id": "df021288-bdef-4463-88db-98f22de89214",
-          "type": "Role"
-        },
-        {
           "id": "741f803b-c850-494e-b5df-cde7c675a1ca",
           "type": "Role"
+        }
+      ]
+    },
+    {
+      "resourceAppId": "00000002-0000-0000-c000-000000000000",
+      "resourceAccess": [
+        {
+          "id": "78c8a3c8-a07e-4b9e-af1b-b5ccab50a175",
+          "type": "Role"
         },
         {
-          "id": "09850681-111b-4a89-9bed-3f2cae46d706",
+          "id": "5778995a-e1bf-45b8-affa-663a9f3f4d04",
+          "type": "Role"
+        },
+        {
+          "id": "1cda74f2-2616-4834-b122-5cb1b07f8a59",
           "type": "Role"
         }
       ]
@@ -56,7 +44,5 @@ Setup steps:
 4. Grant permissions to the app in the partner directory
 5. Pre-consent the app's service principal: https://blogs.msdn.microsoft.com/iwilliams/2017/12/01/cloud-solution-provider-pre-consent/
 6. Create a key for the application, place it in PartnerTest/App.Config: `BootstrapAppSecret`
-7. Create a new customer tenant from the Partner Center UI
-8. Wait 3 minutes
-9. Put the application id and tenant directory id in PartnerTest/Program.cs
-10. Run the console application, observe the error `The identity of the calling application could not be established.`
+7. Put the application id, subscription id, tenant directory id in PartnerTest/Program.cs
+8. Run the console application, observe the error `The identity of the calling application could not be established.`
